efb00b35eacde241d1961be0fedf0f7c825852f5
Quentin Fan-Chiang <userblackbox@tutanota.com>
2020-07-23 12:32:38 -0400

    xdotool fixes

--- key-runner.py
+++ key-runner.py
@@ -4,6 +4,9 @@ import os
 key = input().split()[0][:-1].split('+')
 for i in range(len(key)):
     if len(key[i]) == 1:
-        key[i].lower
+        key[i] = key[i].lower()
+    elif key[i].lower() in ('ctrl','super','alt', 'shift'):
+        key[i] = key[i].lower()
+key = '+'.join(key)
 
 os.system('xdotool key ' + key)
